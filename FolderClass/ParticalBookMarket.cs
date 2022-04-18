5618b433e6301da44efe1862d4da1c0b581cdea5
Saha <t343st@gmail.com>
2022-04-18 10:06:19 +0300

    Сдлал вывод кол-ва книг в корзине

--- FolderClass/ParticalBookMarket.cs
+++ FolderClass/ParticalBookMarket.cs
@@ -9,65 +9,65 @@ namespace WPF_EX2022
     public partial class BookMarket
     {
 
-        //#region Proprty
-        //public string TitleGener
-        //{
-        //    get
-        //    {
-        //        return Title + " | " + Genre;
-        //    }
-        //}
+        #region Proprty
+        public string TitleGener
+        {
+            get
+            {
+                return Title + " | " + Genre;
+            }
+        }
 
-        //public string StockCount
-        //{
-        //    get
-        //    {
-        //        return Count(CountInStock);
-        //    }
-        //}
+        public string StockCount
+        {
+            get
+            {
+                return Count(CountInStock);
+            }
+        }
 
-        //public string MarketCount
-        //{
-        //    get
-        //    {
-        //        return Count(CountInMarket);
-        //    }
-        //}
+        public string MarketCount
+        {
+            get
+            {
+                return Count(CountInMarket);
+            }
+        }
 
-        //public string Img
-        //{
-        //    get
-        //    {
-        //        return "\\"+Image;
-        //    }
-        //}
+        public string Img
+        {
+            get
+            {
+                return "\\" + Image;
+            }
+        }
 
-        //public string SubDiscription
-        //{
-        //    get
-        //    {
-        //        return Discription;
-        //    }
-        //}
-        //#endregion
+        public string SubDiscription
+        {
+            get
+            {
+                return Discription;
+            }
+        }
+        #endregion
 
-        //#region Methods
-        //public string Count(int count )
-        //{
-        //    if (count == 0)
-        //    {
-        //        return "нет";
-        //    }
-        //    if (count> 5)
-        //    {
-        //        return "много";
-        //    }
-        //    else
-        //    {
-        //        return ""+ count;
-        //    }
-        //    return "";
-        //}
-        //#endregion
+        #region Methods
+        public string Count(int count)
+        {
+            if (count == 0)
+            {
+                return "нет";
+            }
+            if (count > 5)
+            {
+                return "много";
+            }
+            else
+            {
+                return "" + count;
+            }
+            return "";
+        }
+        #endregion
     }
 }
--- FolderPage/MainPage.xaml.cs
+++ FolderPage/MainPage.xaml.cs
@@ -20,10 +20,50 @@ namespace WPF_EX2022.FolderPage
     /// </summary>
     public partial class MainPage : Page
     {
+        List<OrderTable> orderTables = new List<OrderTable>();
+        List<BookMarket> bookMarkets = FolderClass.BD.Data.BookMarket.ToList();
+
         public MainPage()
         {
             InitializeComponent();
             LVmarket.ItemsSource = FolderClass.BD.Data.BookMarket.ToList();
         }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button B = (Button)sender;
+            int ind = Convert.ToInt32(B.Uid);
+
+            OrderTable UsOrder = new OrderTable();
+            List<OrderTable> orders = FolderClass.BD.Data.OrderTable.ToList();
+
+            UsOrder.BookId = ind;
+            int MaxCount = bookMarkets[ind-1].CountInMarket + bookMarkets[ind-1].CountInStock;
+            int UsCount = 0;
+
+            for (int i = 0; i < orders.Count; i++)
+            {
+                if (orders[i].BookId == ind)
+                {
+                    UsCount++;
+                }
+            }
+            if (UsCount >= MaxCount)
+            {
+                MessageBox.Show("Информация", "Больше заказать нельзя", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                FolderClass.BD.Data.OrderTable.Add(UsOrder);
+                FolderClass.BD.Data.SaveChanges();
+                orderTables = FolderClass.BD.Data.OrderTable.ToList();
+                MessageBox.Show("Добавление", "Данные добавлены в корзину", MessageBoxButton.OK, MessageBoxImage.Information);
+                TBOXCountBook.Text = "Колличество выбранных книг: "+ orderTables.Count+"";
+            }           
+        }
+
+
     }
 }
